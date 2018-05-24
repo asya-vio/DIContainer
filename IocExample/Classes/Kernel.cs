c83f9be1f88c3c759b2a55e2f2bf1eb6aa78427e
Anastasia Fedina <fedinastasia@gmail.com>
2018-05-24 10:55:01 +0500

    Initial

---
+++ IocExample/Classes/Kernel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocExample
+{
+    class Kernel
+    {
+        Dictionary<Type, Type> dictType = new Dictionary<Type, Type>();
+
+        Dictionary<Type, Object> dictTypeObj = new Dictionary<Type, Object>();
+        public void Bind(Type bind, Type to)
+        {
+            dictType.Add(bind, to);
+
+        }
+
+        public void BindToObject(Type bind, Object obj )
+        {
+            dictTypeObj.Add(bind, obj);
+
+        }
+
+        public T Get<T>()
+        {
+            return (T)Get(typeof(T));
+
+
+        }
+
+        private object Get(Type getType)
+        {
+            if (dictTypeObj.ContainsKey(getType))
+            {
+                return dictTypeObj[getType];
+            }
+            else
+            {
+                getType = dictType[getType];
+
+                var constrInfo = Utils.GetSingleConstructor(getType);
+                List<Object> parameters = new List<object>();
+                foreach (var parameter in constrInfo.GetParameters())
+                {
+                    parameters.Add(Get(parameter.ParameterType));
+                }
+
+                return Utils.CreateInstance(getType, parameters);
+
+            } 
+        }
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,67 @@
+﻿using IocExample.Classes;
+using Ninject;
+using System;
+namespace IocExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var logger = new ConsoleLogger();
+        //    var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+        //    var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+        //    createUserHandler.Handle();
+        //}
+
+        //1
+        //static void Main(string[] args)
+        //{
+        //    IKernel kernel = new StandardKernel();
+
+        //    kernel.Bind<ILogger>().To<ConsoleLogger>();
+        //    kernel.Bind<UserService>().To<UserService>();
+        //    kernel.Bind<QueryExecutor>().To<QueryExecutor>();
+        //    kernel.Bind<CommandExecutor>().To<CommandExecutor>();
+        //    kernel.Bind<CacheService>().To<CacheService>();
+        //    kernel.Bind<RestClient>()
+        //        .ToConstructor(k => new RestClient("API_KEY"));
+        //    kernel.Bind<CreateUserHandler>().To<CreateUserHandler>();
+        //    kernel.Bind<IConnectionFactory>()
+        //        .ToConstructor(k => new SqlConnectionFactory("SQL Connection", k.Inject<ILogger>()))
+        //        .InSingletonScope();
+
+        //    var createUserHandler = kernel.Get<CreateUserHandler>();
+
+        //    createUserHandler.Handle();
+        //    Console.ReadKey();
+
+        //}
+
+        //2
+        static void Main(string[] args)
+        {
+
+            var kernel = new Kernel();
+
+            kernel.Bind(typeof(ILogger), typeof(ConsoleLogger));
+            kernel.Bind(typeof(UserService), typeof(UserService));
+            kernel.Bind(typeof(QueryExecutor), typeof(QueryExecutor));
+            kernel.Bind(typeof(CommandExecutor), typeof(CommandExecutor));
+            kernel.Bind(typeof(CacheService), typeof(CacheService));
+            kernel.Bind(typeof(CreateUserHandler), typeof(CreateUserHandler));
+
+            kernel.BindToObject(typeof(IConnectionFactory), new SqlConnectionFactory("SQL Connection", kernel.Get<ILogger>()));
+
+            kernel.BindToObject(typeof(RestClient), new RestClient("API_KEY"));
+
+            var createUserHandler = kernel.Get<CreateUserHandler>();
+
+
+
+            createUserHandler.Handle();
+            Console.ReadKey();
+
+        }
+    }
+}
